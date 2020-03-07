aead8fca8c4261216cd62f685e66904872e85162
thealk <thea.knowles@gmail.com>
2020-03-07 11:37:55 -0500

    updated 1_tidy.R & added simulated VOT from Politzer-Ahles & Piccinini 2018

--- presentations/2_materials/1_tidy.R
+++ presentations/2_materials/1_tidy.R
@@ -10,15 +10,30 @@ library(readxl)
 
 # Load data ----
 # Read in the data using the read_excel function from the readxl package
-vot_data <- read_excel("presentations/2_materials/casa_lab_vot_data.xlsx")
+vot <- read_excel("presentations/2_materials/casa_lab_vot_data.xlsx")
+vot <- read.csv("presentations/2_materials/simulated_vot_data.csv")
+
+
+# OR USE SIMULATED VOT DATA FROM POLITZER-AHLES & PICCINIINI
+# https://osf.io/2n3mu/
 
 # Explore data ----
 # Take a look at the data using head(), str(), and summary() functions
-head(vot_data)
+head(vot) # Take a peak at the first 6 rows
+str(vot) # Take a look at the structure of each variable
+names(vot) # What are the names of the variables (columns)?
+summary(vot) # Summary stats for all variables
+nrow(vot) # How many rows?
+View(vot) # View in spreadsheet-like form
 
-str(vot_data)
+# Explore data by variables ----
+head(vot$Participant) # first 6 values of the variable Participant
+levels(vot$Participant) # all "levels" of the variable Participant, which is a factor
+length(levels(vot$Participant)) # how many levels are there?
 
-summary(vot_data)
+mean(vot$VOT)
+min(vot$VOT)
+max(vot$VOT)
 
 # Prepare data ----
 # Now we'll start to use some tidyverse syntax
